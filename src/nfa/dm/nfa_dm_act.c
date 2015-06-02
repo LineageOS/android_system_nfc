a1ce313dd04f4257232eecd3787c33f19a27dd6c
Jizhou Liao <Jizhou.Liao@nxp.com>
2015-06-02 15:33:54 -0700

    Memory leak and NULL pointer check fixes
    Change-Id: Idf720890d5b0307d22407b3a28929b8a72d4404f

--- src/nfa/dm/nfa_dm_act.c
+++ src/nfa/dm/nfa_dm_act.c
@@ -1630,6 +1630,11 @@ static void nfa_dm_poll_disc_cback (tNFA_DM_RF_DISC_EVT event, tNFC_DISCOVER *p_
                 {
                     /* activate LLCP */
                     nfa_p2p_activate_llcp (p_data);
+                    if (nfa_dm_cb.p_activate_ntf)
+                    {
+                        GKI_freebuf (nfa_dm_cb.p_activate_ntf);
+                        nfa_dm_cb.p_activate_ntf = NULL;
+                    }
                 }
                 else
                 {
