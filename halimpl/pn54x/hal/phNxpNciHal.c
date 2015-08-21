ae2234d1cdd7c35543a795dcb51a6537ffce2007
Jizhou Liao <Jizhou.Liao@nxp.com>
2015-08-21 16:32:46 -0700

    add recovery mechanism on CORE_RESET_NTF
    The recovery re-init NFC by keeping configuration.
    This recovers P2P issue while CORE_RESET_NTF is received.
    
    Bug: 23520630
    Change-Id: If2296558064029972b61823861d47e47400cc8dc

--- halimpl/pn54x/hal/phNxpNciHal.c
+++ halimpl/pn54x/hal/phNxpNciHal.c
@@ -56,6 +56,8 @@ extern int send_to_upper_kovio;
 extern int kovio_detected;
 extern int disable_kovio;
 static uint8_t Rx_data[NCI_MAX_DATA_LEN];
+uint8_t discovery_cmd[50] = { 0 };
+uint8_t discovery_cmd_len = 0;
 extern bool_t rf_deactive_cmd;
 
 uint32_t timeoutTimerId = 0;
@@ -460,6 +462,9 @@ int phNxpNciHal_open(nfc_stack_callback_t *p_cback, nfc_stack_data_callback_t *p
     = (uintptr_t) nxpncihal_ctrl.gDrvCfg.nClientId;
     tOsalConfig.pLogFile = NULL;
     tTmlConfig.dwGetMsgThreadId = (uintptr_t) nxpncihal_ctrl.gDrvCfg.nClientId;
+
+    memset (discovery_cmd, 0, sizeof(discovery_cmd));
+    discovery_cmd_len = 0;
 
     /* Initialize TML layer */
     wConfigStatus = phTmlNfc_Init(&tTmlConfig);
@@ -2442,5 +2447,80 @@ static void phNxpNciHal_print_res_status( uint8_t *p_rx_data, uint16_t *p_len)
             NXPLOG_NCIHAL_W ("Invalid Data from config file . Aborting..");
             phNxpNciHal_close ();
         }
+    }
+}
+
+NFCSTATUS phNxpNciHal_core_reset_recovery ()
+{
+    NFCSTATUS status = NFCSTATUS_FAILED;
+
+    uint8_t buffer[260];
+    long bufflen = 260;
+
+    /*NCI_INIT_CMD*/
+    static uint8_t cmd_init_nci[] = {0x20, 0x01, 0x00};
+    /*NCI_RESET_CMD*/
+    static uint8_t cmd_reset_nci[] = {0x20, 0x00, 0x01, 0x00}; //keep configuration
+
+    /* reset config cache */
+    uint8_t retry_core_init_cnt = 0;
+
+    if (discovery_cmd_len == 0)
+    {
+        goto FAILURE;
+    }
+    NXPLOG_NCIHAL_D ("%s: recovery", __FUNCTION__);
+
+retry_core_init:
+    if (retry_core_init_cnt > 3)
+    {
+        goto FAILURE;
+    }
+
+    status = phTmlNfc_IoCtl (phTmlNfc_e_ResetDevice);
+    if (status != NFCSTATUS_SUCCESS)
+    {
+        NXPLOG_NCIHAL_D("PN54X Reset - FAILED\n");
+        goto FAILURE;
+    }
+    status = phNxpNciHal_send_ext_cmd (sizeof(cmd_reset_nci), cmd_reset_nci);
+    if ((status != NFCSTATUS_SUCCESS) && (nxpncihal_ctrl.retry_cnt >= MAX_RETRY_COUNT))
+    {
+        retry_core_init_cnt++;
+        goto retry_core_init;
+    }
+    else if (status != NFCSTATUS_SUCCESS)
+    {
+        NXPLOG_NCIHAL_D ("NCI_CORE_RESET: Failed");
+        retry_core_init_cnt++;
+        goto retry_core_init;
+    }
+    status = phNxpNciHal_send_ext_cmd (sizeof(cmd_init_nci), cmd_init_nci);
+    if (status != NFCSTATUS_SUCCESS)
+    {
+        NXPLOG_NCIHAL_D ("NCI_CORE_INIT : Failed");
+        retry_core_init_cnt++;
+        goto retry_core_init;
+    }
+
+    status = phNxpNciHal_send_ext_cmd (discovery_cmd_len, discovery_cmd);
+    if (status != NFCSTATUS_SUCCESS)
+    {
+        NXPLOG_NCIHAL_D ("RF_DISCOVERY : Failed");
+        retry_core_init_cnt++;
+        goto retry_core_init;
+    }
+    return NFCSTATUS_SUCCESS;
+FAILURE:
+    abort ();
+}
+
+void phNxpNciHal_discovery_cmd_ext (uint8_t *p_cmd_data, uint16_t cmd_len)
+{
+    NXPLOG_NCIHAL_D ("phNxpNciHal_discovery_cmd_ext");
+    if (cmd_len > 0 && cmd_len <= sizeof(discovery_cmd))
+    {
+        memcpy (discovery_cmd, p_cmd_data, cmd_len);
+        discovery_cmd_len = cmd_len;
     }
 }
