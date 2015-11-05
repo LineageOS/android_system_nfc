c56a929eeb9856d5fd78825c00398ba0d471c816
Martijn Coenen <maco@google.com>
2015-11-05 11:10:12 +0100

    Don't free memory that shouldn't be freed.
    In case of NFC_ERROR_CEVT, p_data is just
    a pointer to an uint8_t, and doesn't contain
    data that needs to be freed. Note that there's
    a fall-through from NFC_DATA_CEVT above which
    warrants further investigation, but for
    now this is the safe fix.
    
    Bug: 25489121
    Change-Id: Ibab5b42ca9defca04b2310c8d9a441c89f0f722d

--- src/nfc/tags/rw_t1t.c
+++ src/nfc/tags/rw_t1t.c
@@ -295,12 +295,6 @@ void rw_t1t_conn_cback (UINT8 conn_id, tNFC_CONN_EVT event, tNFC_CONN *p_data)
         {
             rw_t1t_process_error ();
         }
-        if((p_data != NULL) && (p_data->data.p_data != NULL))
-        {
-            /* Free the response buffer in case of invalid response*/
-            GKI_freebuf((BT_HDR *) (p_data->data.p_data));
-            p_data->data.p_data = NULL;
-        }
         break;
 
     default:
