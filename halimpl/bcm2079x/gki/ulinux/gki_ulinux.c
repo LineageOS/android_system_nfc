ca6e0e1f81370aeb94c2d3407a33f8040ae57c90
Ruchi Kandoi <kandoiruchi@google.com>
2017-04-14 13:08:16 -0700

    bcm2079x: Replace pthread_exit with return to avoid memory leak
    Test: Android Malloc Debug and dumpsys meminfo
    Bug: 35680718
    Change-Id: I7cea8141da7c11a054970044749b5c7b39448a3b
    Signed-off-by: Ruchi Kandoi <kandoiruchi@google.com>

--- halimpl/bcm2079x/gki/ulinux/gki_ulinux.c
+++ halimpl/bcm2079x/gki/ulinux/gki_ulinux.c
@@ -112,7 +112,7 @@ void gki_task_entry(uintptr_t params) {
   GKI_TRACE_ERROR_1("gki_task task_id=%i terminating", p_pthread_info->task_id);
   gki_cb.os.thread_id[p_pthread_info->task_id] = 0;
 
-  pthread_exit(0); /* GKI tasks have no return value */
+  return;
 }
 /* end android */
 
@@ -450,7 +450,7 @@ void timer_thread(signed long id) {
     GKI_timer_update(1);
   }
   GKI_TRACE_ERROR_1("%s exit", __func__);
-  pthread_exit(NULL);
+  return;
 }
 #endif
 
@@ -622,7 +622,6 @@ uint16_t GKI_wait(uint16_t flag, uint32_t timeout) {
   if (rtask >= GKI_MAX_TASKS) {
     GKI_TRACE_ERROR_3("%s() Exiting thread; rtask %d >= %d", __func__, rtask,
                       GKI_MAX_TASKS);
-    pthread_exit(NULL);
     return 0;
   }
 
@@ -712,7 +711,6 @@ uint16_t GKI_wait(uint16_t flag, uint32_t timeout) {
       GKI_TRACE_ERROR_1("GKI TASK_DEAD received. exit thread %d...", rtask);
 
       gki_cb.os.thread_id[rtask] = 0;
-      pthread_exit(NULL);
       return (EVENT_MASK(GKI_SHUTDOWN_EVT));
     }
   }
