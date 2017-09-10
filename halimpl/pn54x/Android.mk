93830203e110f8055104789df3107eae5aeb789d
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-10 13:56:47 -0400

    nfc: Set nq1xx chips to pn547 and nq2xx to pn548
    Change-Id: I5c75549a6975879f397b596941fa5f0cde0500e2

--- halimpl/pn54x/Android.mk
+++ halimpl/pn54x/Android.mk
@@ -36,9 +36,9 @@ D_CFLAGS += -DPN553=4
 endif
 
 #### Select the CHIP ####
-ifeq ($(BOARD_NFC_CHIPSET),pn547)
+ifneq ($(filter pn547 nq110 nq120,$(BOARD_NFC_CHIPSET)),)
 NXP_CHIP_TYPE := $(PN547C2)
-else ifeq ($(BOARD_NFC_CHIPSET),pn548)
+else ifneq ($(filter pn548 nq210 nq220,$(BOARD_NFC_CHIPSET)),)
 NXP_CHIP_TYPE := $(PN548C2)
 else ifeq ($(BOARD_NFC_CHIPSET),pn551)
 NXP_CHIP_TYPE := $(PN551)
