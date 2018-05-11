e9fe7e9fa569a20c7d70d9ea5adcc8f54b2a7e29
anil.hiranniah <anil.hiranniah@nxp.com>
2018-05-11 20:48:27 +0530

    NCI2.0: Handling NFCEE_STATUS_NTF(unrecoverable error)
    After reception of NFCEE_STATUS_NTF (Unrecoverable Error),
    Host shall send  NFCEE_DISCOVER_CMD and then
    send NFCEE_MODE_SET_CMD to send eSE to enabled state.
    
    Bug: 69876039
    Bug: 80370223
    Test: Send dummy NFCEE_STATUS_NTF and check for recovery.
    NFC Enable/Disable
    
    Change-Id: I8d1d5f75a29b7135137fb7d57ca1f892b1e885fc

--- src/nfa/ee/nfa_ee_main.cc
+++ src/nfa/ee/nfa_ee_main.cc
@@ -77,7 +77,8 @@ const tNFA_EE_SM_ACT nfa_ee_actions[] = {
     nfa_ee_nci_wait_rsp,        /* NFA_EE_NCI_WAIT_RSP_EVT      */
     nfa_ee_rout_timeout,        /* NFA_EE_ROUT_TIMEOUT_EVT      */
     nfa_ee_discv_timeout,       /* NFA_EE_DISCV_TIMEOUT_EVT     */
-    nfa_ee_lmrt_to_nfcc         /* NFA_EE_CFG_TO_NFCC_EVT       */
+    nfa_ee_lmrt_to_nfcc,        /* NFA_EE_CFG_TO_NFCC_EVT       */
+    nfa_ee_nci_nfcee_status_ntf /*NFA_EE_NCI_NFCEE_STATUS_NTF_EVT*/
 };
 
 /*******************************************************************************
@@ -346,6 +347,10 @@ void nfa_ee_proc_evt(tNFC_RESPONSE_EVT event, void* p_data) {
     case NFC_SET_ROUTING_REVT:
       int_event = NFA_EE_NCI_WAIT_RSP_EVT;
       cbk.opcode = NCI_MSG_RF_SET_ROUTING;
+      break;
+
+    case NFC_NFCEE_STATUS_REVT:
+      int_event = NFA_EE_NCI_NFCEE_STATUS_NTF_EVT;
       break;
   }
 
