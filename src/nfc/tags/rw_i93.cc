bf1b468f2bd186cad66915637ccdf9d6578ccc47
Ruchi Kandoi <kandoiruchi@google.com>
2019-01-24 15:27:47 -0800

    Prevent OOB read in rw_i93_sm_format()
    Test: Nfc Disable/Enable
    Bug: 122323053
    Change-Id: Iac1e71c56cbbf16900a83a20e3c296185eca9fff

--- src/nfc/tags/rw_i93.cc
+++ src/nfc/tags/rw_i93.cc
@@ -2405,6 +2405,10 @@ void rw_i93_sm_format(NFC_HDR* p_resp) {
       "sub_state:%s (0x%x)",
       rw_i93_get_sub_state_name(p_i93->sub_state).c_str(), p_i93->sub_state);
 
+  if (length == 0) {
+    android_errorWriteLog(0x534e4554, "122323053");
+    return;
+  }
   STREAM_TO_UINT8(flags, p);
   length--;
 
@@ -2431,6 +2435,10 @@ void rw_i93_sm_format(NFC_HDR* p_resp) {
   switch (p_i93->sub_state) {
     case RW_I93_SUBSTATE_WAIT_UID:
 
+      if (length < (I93_UID_BYTE_LEN + 1)) {
+        android_errorWriteLog(0x534e4554, "122323053");
+        return;
+      }
       p++; /* skip DSFID */
       p_uid = p_i93->uid;
       STREAM_TO_ARRAY8(p_uid, p); /* store UID */
